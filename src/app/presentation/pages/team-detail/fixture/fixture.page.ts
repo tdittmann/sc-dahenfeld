1c12aaee0b33f565dd0dda1001de21b60518ea00
Timo Dittmann <timo.dittmann@kaufland.com>
2019-09-26 18:13:06 +0200

    Added initial feature to scroll to next match

--- src/app/presentation/pages/team-detail/fixture/fixture.page.ts
+++ src/app/presentation/pages/team-detail/fixture/fixture.page.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FixtureService} from '../../../../dataproviders/soccer/fixture/fixture.service';
 import {FixtureMatch} from '../../../../core/domain/fixtureMatch.model';
+import {DateUtils} from '../../../../util/DateUtils';
 
+// TODO tdit0703: Scroll to next match
 @Component({
     selector: 'fixture',
     templateUrl: 'fixture.page.html',
@@ -11,6 +13,7 @@ import {FixtureMatch} from '../../../../core/domain/fixtureMatch.model';
 export class FixturePage implements OnInit {
 
     matches: FixtureMatch[] = [];
+    nextMatchId = 0;
 
     constructor(private route: ActivatedRoute,
                 private fixtureService: FixtureService) {
@@ -25,6 +28,7 @@ export class FixturePage implements OnInit {
                 this.fixtureService.loadFixture(teamId).subscribe(
                     fixtureMatches => {
                         this.matches = fixtureMatches;
+                        this.nextMatchId = this.getNextMatchId(this.matches);
                     },
                     error => {
                         // TODO tdit0703: Error handling
@@ -33,6 +37,28 @@ export class FixturePage implements OnInit {
                 );
             }
         );
+    }
+
+    private getNextMatchId(matches: FixtureMatch[]): number {
+        let actualMatchId: number;
+        let lastMatchId: number;
+        let actualMatchdayDate = DateUtils.ofIsoDate('9999-12-31');
+
+        for (let i = 0; i < matches.length; i++) {
+            lastMatchId = matches[i].id;
+
+            if (isNaN(matches[i].homeResult) && matches[i].date.isSameOrBefore(actualMatchdayDate)) {
+                actualMatchId = matches[i].id;
+                actualMatchdayDate = matches[i].date;
+            }
+        }
+
+        // Fallback: use latest match
+        if (!actualMatchId) {
+            actualMatchId = lastMatchId;
+        }
+
+        return actualMatchId;
     }
 
 }
