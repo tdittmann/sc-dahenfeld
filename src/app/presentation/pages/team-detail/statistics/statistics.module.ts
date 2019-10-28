223a10401c353113be06b38d3965a03bd653d211
Timo Dittmann <timo.dittmann@kaufland.com>
2019-10-28 20:39:56 +0100

    Added todo

--- src/app/presentation/pages/team-detail/statistics/statistics.module.ts
+++ src/app/presentation/pages/team-detail/statistics/statistics.module.ts
@@ -4,6 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 import {StatisticsPage} from './statistics.page';
 
+// TODO tdit0703: Add to tab routing
 @NgModule({
     imports: [
         CommonModule,
