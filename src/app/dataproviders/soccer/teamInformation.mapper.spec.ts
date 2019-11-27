148a5629ee89b07853b15835b2d170940a20a403
Timo Dittmann <timo.dittmann@kaufland.com>
2019-11-27 07:54:42 +0100

    Fixed tests

--- src/app/dataproviders/soccer/teamInformation.mapper.spec.ts
+++ src/app/dataproviders/soccer/teamInformation.mapper.spec.ts
@@ -1,6 +1,6 @@
 import {TeamInformationMapper} from './teamInformation.mapper';
-import {TeamInformationJson} from './teamInformationJson.model';
-import {TeamInformation} from '../../core/domain/teamInformation.model';
+import {TeamInformationJson, TeamInformationSeasonJson} from './teamInformationJson.model';
+import {TeamInformation, TeamInformationSeason} from '../../core/domain/teamInformation.model';
 
 describe('TeamInformationMapper', () => {
 
@@ -15,13 +15,27 @@ describe('TeamInformationMapper', () => {
     });
 
     it('should map from json to core model', () => {
+        const actualSeasons: TeamInformationSeasonJson[] = [
+            {id: '90', name: '13/14'},
+            {id: '91', name: '14/15'},
+            {id: '92', name: '15/16'}
+        ];
+
         const actual: TeamInformationJson = {
             name: '1. Mannschaft',
             showRanking: true,
             showFixture: false,
             showPlayers: true,
             showStatistics: false,
+            showSeasons: true,
+            seasons: actualSeasons
         };
+
+        const expectedSeasons: TeamInformationSeason[] = [
+            {projectId: 90, name: '13/14'},
+            {projectId: 91, name: '14/15'},
+            {projectId: 92, name: '15/16'}
+        ];
 
         const expected: TeamInformation = new TeamInformation();
         expected.name = '1. Mannschaft';
@@ -29,6 +43,8 @@ describe('TeamInformationMapper', () => {
         expected.showFixture = false;
         expected.showPlayers = true;
         expected.showStatistics = false;
+        expected.showSeasons = true;
+        expected.seasons = expectedSeasons;
 
         expect(mapper.mapFrom(actual)).toEqual(expected);
     });
