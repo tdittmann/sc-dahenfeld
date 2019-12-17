ba191050d45435a724b30618cae7f6207f677aae
Timo Dittmann <timo.dittmann@kaufland.com>
2019-12-17 08:24:53 +0100

    Added link to home when clicking on logo in page-header

--- src/app/presentation/shared/page-header/page-header.module.ts
+++ src/app/presentation/shared/page-header/page-header.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PageHeaderComponent} from './page-header.component';
 import {IonicModule} from '@ionic/angular';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
     providers: [],
     imports: [
         CommonModule,
         IonicModule,
+        RouterModule,
     ],
     declarations: [PageHeaderComponent],
     exports: [PageHeaderComponent]
