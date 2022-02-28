c242ea2217d47e38448188cf042e74b0ddba3484
Timo Dittmann <Timo.Dittmann@mail.schwarz>
2022-02-28 07:48:00 +0100

    Dependency updates

--- src/app/presentation/pages/imprint/imprint.page.ts
+++ src/app/presentation/pages/imprint/imprint.page.ts
@@ -16,7 +16,7 @@ import {App} from '@capacitor/app';
 export class ImprintPage implements OnInit {
 
     heading: string;
-    version = '5.11.2';
+    version = '5.12.0';
     developer = 'Timo Dittmann';
     darkMode = false;
 
