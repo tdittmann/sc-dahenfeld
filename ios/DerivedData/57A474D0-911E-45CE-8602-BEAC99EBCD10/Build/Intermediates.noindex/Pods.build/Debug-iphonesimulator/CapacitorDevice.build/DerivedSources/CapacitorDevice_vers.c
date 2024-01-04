bc94ba9eb734847e8803f42ddadebb0109935356
Timo Dittmann <Timo.Dittmann@mail.schwarz>
2024-01-04 14:14:18 +0100

    Updated iOS

--- ios/DerivedData/57A474D0-911E-45CE-8602-BEAC99EBCD10/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorDevice.build/DerivedSources/CapacitorDevice_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CapacitorDeviceVersionString[];
- extern const double CapacitorDeviceVersionNumber;
-
- const unsigned char CapacitorDeviceVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CapacitorDevice  PROJECT:Pods-1" "\n";
- const double CapacitorDeviceVersionNumber __attribute__ ((used)) = (double)1.;
--- ios/DerivedData/57A474D0-911E-45CE-8602-BEAC99EBCD10/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/CapacitorPushNotifications.build/DerivedSources/CapacitorPushNotifications_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char CapacitorPushNotificationsVersionString[];
- extern const double CapacitorPushNotificationsVersionNumber;
-
- const unsigned char CapacitorPushNotificationsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:CapacitorPushNotifications  PROJECT:Pods-1" "\n";
- const double CapacitorPushNotificationsVersionNumber __attribute__ ((used)) = (double)1.;
