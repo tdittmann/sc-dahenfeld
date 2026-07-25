fae4abeb363922d6679ba5047499a6b77b89656a
Timo Dittmann <timo.dittmann@mail.schwarz>
2026-07-25 20:48:04 +0200

    [6.13.0] Increase targetSDK for Android

--- src/app/presentation/pages/imprint/imprint.page.ts
+++ src/app/presentation/pages/imprint/imprint.page.ts
@@ -25,7 +25,7 @@ export class ImprintPage implements OnInit {
   private readonly profileService = inject(ProfileService);
 
   heading: string;
-  version = '6.12.0';
+  version = '6.13.0';
   developer = 'Timo Dittmann';
   darkMode = false;
 
